6894d076528e478ebd5548947112b564cfa17fd8
mojoee <47809126+mojoee@users.noreply.github.com>
2023-04-09 12:24:14 +0800

    added math functions and basic functions

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 ## Description
 
 This project is the final project of the odin project, where the goal is to develop a basic calculator, that can do all important mathematical operations.
+
---
+++ math.js
@@ -0,0 +1,21 @@
+function divide(num1, num2) {
+    if(num2 === 0) {
+      return "Cannot divide by zero";
+    }
+    return num1 / num2;
+  }
+
+
+function multiply(num1, num2) {
+    return num1 * num2;
+  }
+
+  function subtract(num1, num2) {
+    return num1 - num2;
+  }
+  
+
+  function add(num1, num2) {
+    return num1 + num2;
+  }
+  
